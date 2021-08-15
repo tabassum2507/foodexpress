88595cd68e8136645ba52bbc77dbdde8f083d317
tabassum2507 <10tabassum2@gmail.com>
2021-08-15 15:23:55 +0530

    new files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        pink: '	#FF69B4'
+
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
